6b2b7001ec2658b02973ed7406197c5463042b05
pengyou <pengyou@pengyou-Aspire-M5-481G.(none)>
2016-07-02 17:10:59 +0800

    first commit

---
+++ trips/views.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=str(pk))
+    return render(request, 'post.html', {'post': post})
